d015561e079fc58eab60a44824f7f2aff91b722d
Benjamin Lu <benlumia007@gmail.com>
2019-02-04 23:31:20 -0800

    setup .csr

--- setup.sh
+++ setup.sh
@@ -67,7 +67,7 @@ if [[ ! -d "/vagrant/certificates/${DOMAIN}" ]]; then
     sed -i -e "s/{{DOMAIN}}/${DOMAIN}/g" "/vagrant/certificates/${DOMAIN}/${DOMAIN}.ext"
 
     noroot openssl genrsa -out "/vagrant/certificates/${DOMAIN}/${DOMAIN}.key" 4096
-    # noroot openssl req -new -key "/vagrant/certificates/${DOMAIN}/dev.key" -out "/vagrant/certificates/${DOMAIN}/dev.csr" -subj "/CN=${DOMAIN}"
+    noroot openssl req -new -key "/vagrant/certificates/${DOMAIN}/${DOMAIN}.key" -out "/vagrant/certificates/${DOMAIN}/${DOMAIN}.csr" -subj "/CN=${DOMAIN}"
     # noroot openssl x509 -req -in "/vagrant/certificates/${DOMAIN}/dev.csr" -CA "/vagrant/certificates/ca/ca.key" -CAcreateserial -out "/vagrant/certificates/${DOMAIN}/dev.crt" -days 3650 -sha256 -extfile "/vagrant/certificates/${DOMAIN}/${DOMAIN}.ext"
     # noroot openssl x509 -req -in "/vagrant/certificates/$domain.csr -CA /certificates/$root/$root.pem -CAkey /certificates/$root/$root.key -CAcreateserial -out $domain.crt -days 3650 -sha256 -extfile $domain.ext
 
