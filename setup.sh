a6b7af186c15257f1d912a80435bb761030e41e5
Benjamin Lu <benlumia007@gmail.com>
2019-02-04 19:35:07 -0800

    add databse and download and install wordpress

--- setup.sh
+++ setup.sh
@@ -20,4 +20,20 @@ if [[ ! -f "/srv/www/${DOMAIN}/public_html/wp-config-sample.php" ]]; then
     cp "/srv/config/wordpress/wp-config.php" "/srv/www/${DOMAIN}/public_html/wp-config.php"
     cd ${VM_DIR}/public_html
     noroot wp core download
+
+    # Setup MySQL Database
+    noroot mysql -uroot -e "CREATE USER '${DOMAIN}'@'localhost' IDENTIFIED BY '${DOMAIN}';"
+    noroot mysql -uroot -e "GRANT USAGE ON *.* TO '${DOMAIN}'@'localhost';"
+    noroot mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${DOMAIN};"
+    noroot mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DOMAIN}.* TO '${DOMAIN}'@'localhost';"
+    noroot mysql -uroot -e "FLUSH PRIVILEGES;"
+
+    noroot sed -i "/DB_HOST/s/'[^']*'/'localhost'/2" wp-config.php
+    noroot sed -i "/DB_NAME/s/'[^']*'/'${DOMAIN}'/2" wp-config.php
+    noroot sed -i "/DB_USER/s/'[^']*'/'${DOMAIN}'/2" wp-config.php
+    noroot sed -i "/DB_PASSWORD/s/'[^']*'/'${DOMAIN}'/2" wp-config.php
+
+    # Installing WordPress
+    echo "Install WordPress"
+    noroot wp core install  --url=https://${DOMAIN}.test --title=${DOMAIN} --admin_user=admin --admin_password=password --admin_email=admin@${DOMAIN}.test
 fi
