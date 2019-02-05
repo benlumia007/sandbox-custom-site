76b1595595061912259fa0d1b4eadbecb76a8a08
Benjamin Lu <benlumia007@gmail.com>
2019-02-04 18:08:24 -0800

    initial commit

--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-if [[ ! -f "/srv/www/${DOMAIN}/public_html/wp-config-sample.php" ]]; then
-    cp "/srv/config/wordpress/wp-config.php" "/srv/www/${DOMAIN}/public_html/wp-config.php"
-    noroot wp core download
-fi
-=======
-echo "This is to verify if ${DOMAIN} (example) shows"
->>>>>>> develop
+echo "This is to verify if ${DOMAIN} (example) shows."
