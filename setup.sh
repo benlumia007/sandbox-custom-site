909887f95a4b63cc4c91607e793d8f943dd5b73e
Benjamin Lu <benlumia007@gmail.com>
2019-02-04 21:53:53 -0800

    updated

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # This should create the basic .conf file for a specific site when it is doing a provision.
 if [[ ! -f /etc/apache2/sites-available/${DOMAIN}.conf ]]; then
   echo "Copying apache2.conf    /etc/apache2/sites-available/${DOMAIN}.conf"
-  cp /vagrant/config/apache/apache.conf /etc/apache2/sites-available/${DOMAIN}.conf
-  sed -i -e "s/{{DOMAIN}}/${DOMAIN}/g" /etc/apache2/sites-available/${DOMAIN}.conf
+  cp "/srv/config/apache/apache.conf" "/etc/apache2/sites-available/${DOMAIN}.conf"
+  sed -i -e "s/{{DOMAIN}}/${DOMAIN}/g" "/etc/apache2/sites-available/${DOMAIN}.conf"
   echo "enable ${DOMAIN}"
   a2ensite ${DOMAIN}.conf
   echo "restarting apache server"
@@ -12,7 +12,7 @@ if [[ ! -f /etc/apache2/sites-available/${DOMAIN}.conf ]]; then
 fi
 
 if [[ ! -d ${VM_DIR} ]]; then
-    echo -e "Creating ${VM_DIR}/public_html"
+    echo "Creating ${VM_DIR}/public_html"
     mkdir -p ${VM_DIR}/public_html
 fi
 
