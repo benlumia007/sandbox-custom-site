07f56ff6afba06cd37780c22dcdfff5af2fc3dfb
Benjamin Lu <benlumia007@gmail.com>
2019-02-04 18:36:51 -0800

    add .conf

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "This is to verify if ${DOMAIN} (example) shows.."
+# This should create the basic .conf file for a specific site when it is doing a provision.
+if [[ ! -f /etc/apache2/sites-available/${DOMAIN}.conf ]]; then
+  echo "Copying apache2.conf    /etc/apache2/sites-available/${DOMAIN}.conf"
+  cp /vagrant/config/apache-config/apache.conf /etc/apache2/sites-available/${DOMAIN}.conf
+  sed -i -e "s/{{DOMAIN}}/${DOMAIN}/g" /etc/apache2/sites-available/${DOMAIN}.conf
+  echo "restarting apache server"
+  service apache2 restart
+fi
