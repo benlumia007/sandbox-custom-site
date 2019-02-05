fef577ae6abb801276260cecd91ff96f3f39a3d0
Benjamin Lu <benlumia007@gmail.com>
2019-02-04 18:51:47 -0800

    forgot to enable site

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ if [[ ! -f /etc/apache2/sites-available/${DOMAIN}.conf ]]; then
   echo "Copying apache2.conf    /etc/apache2/sites-available/${DOMAIN}.conf"
   cp /vagrant/config/apache/apache.conf /etc/apache2/sites-available/${DOMAIN}.conf
   sed -i -e "s/{{DOMAIN}}/${DOMAIN}/g" /etc/apache2/sites-available/${DOMAIN}.conf
+  echo "enable ${DOMAIN}"
+  a2ensite ${DOMAIN}.conf
   echo "restarting apache server"
   service apache2 restart
 fi
