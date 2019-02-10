c7059ef0d2eda700e5bf38056313f4d521eae049
Benjamin Lu <benlumia007@gmail.com>
2019-02-09 21:56:09 -0800

    changed all caps to lowercase

--- setup.sh
+++ setup.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # This should create the basic .conf file for a specific site when it is doing a provision.
-if [[ ! -f /etc/apache2/sites-available/${DOMAIN}.conf ]]; then
-  echo "Copying apache2.conf    /etc/apache2/sites-available/${DOMAIN}.conf"
-  cp "/srv/config/apache/apache.conf" "/etc/apache2/sites-available/${DOMAIN}.conf"
-  sed -i -e "s/{{DOMAIN}}/${DOMAIN}/g" "/etc/apache2/sites-available/${DOMAIN}.conf"
-  echo "enable ${DOMAIN}"
-  a2ensite ${DOMAIN}.conf
+if [[ ! -f /etc/apache2/sites-available/${domain}.conf ]]; then
+  echo "Copying apache2.conf    /etc/apache2/sites-available/${domain}.conf"
+  cp "/srv/config/apache/apache.conf" "/etc/apache2/sites-available/${domain}.conf"
+  sed -i -e "s/{{DOMAIN}}/${domain}/g" "/etc/apache2/sites-available/${domain}.conf"
+  echo "enable ${domain}"
+  a2ensite ${domain}.conf
 fi
 
-if [[ ! -d ${VM_DIR}/public_html ]]; then
-    echo "Creating ${VM_DIR}/public_html"
-    mkdir -p ${VM_DIR}/public_html
+if [[ ! -d ${vm_dir}/public_html ]]; then
+    echo "Creating ${vm_dir}/public_html"
+    mkdir -p ${vm_dir}/public_html
 fi
 
-if [[ ! -f "/srv/www/${DOMAIN}/public_html/wp-config-sample.php" ]]; then
-    cp "/srv/config/wordpress/wp-config.php" "/srv/www/${DOMAIN}/public_html/wp-config.php"
-    cd ${VM_DIR}/public_html
+if [[ ! -f "/srv/www/${domain}/public_html/wp-config-sample.php" ]]; then
+    cp "/srv/config/wordpress/wp-config.php" "/srv/www/${domain}/public_html/wp-config.php"
+    cd ${vm_dir}/public_html
     noroot wp core download
 
     # Setup MySQL Database
-    noroot mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${DOMAIN};"
-    noroot mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DOMAIN}.* TO 'wp'@'localhost';"
+    noroot mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${domain};"
+    noroot mysql -uroot -e "GRANT ALL PRIVILEGES ON ${domain}.* TO 'wp'@'localhost';"
     noroot mysql -uroot -e "FLUSH PRIVILEGES;"
 
     noroot sed -i "/DB_HOST/s/'[^']*'/'localhost'/2" wp-config.php
-    noroot sed -i "/DB_NAME/s/'[^']*'/'${DOMAIN}'/2" wp-config.php
+    noroot sed -i "/DB_NAME/s/'[^']*'/'${domain}'/2" wp-config.php
     noroot sed -i "/DB_USER/s/'[^']*'/'wp'/2" wp-config.php
     noroot sed -i "/DB_PASSWORD/s/'[^']*'/'wp'/2" wp-config.php
 
     # Installing WordPress
     echo "Install WordPress"
-    noroot wp core install  --url=https://${DOMAIN}.test --title=${DOMAIN} --admin_user=admin --admin_password=password --admin_email=admin@${DOMAIN}.test
+    noroot wp core install  --url=https://${domain}.test --title=${domain} --admin_user=admin --admin_password=password --admin_email=admin@${domain}.test
     echo
     # Download WordPress Importer
     echo "Download and Install WordPress Installer"
