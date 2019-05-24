361ac600092b9b1f171db1be4f53c2ddd8a1ffcb
Benjamin Lu <benlumia007@gmail.com>
2019-05-24 07:51:08 -0700

    add plugins

--- setup.sh
+++ setup.sh
@@ -32,11 +32,11 @@ if [[ ! -f "/srv/www/${domain}/public_html/wp-config-sample.php" ]]; then
     echo "Install WordPress"
     noroot wp core install  --url=https://${domain}.test --title=${domain} --admin_user=admin --admin_password=password --admin_email=admin@${domain}.test
     noroot wp config shuffle-salts
-fi
 
-plugins=`get_config_value 'plugins' ''`
-if [[ ! -z "${plugins}" ]]; then
-  for plugin in ${plugins//- /$'\n'}; do
-    noroot wp plugin install "${plugin}" --path="/srv/www/${domain}/public_html" --activate
-  done
+    plugins=`get_config_value 'plugins' ''`
+    if [[ ! -z "${plugins}" ]]; then
+      for plugin in ${plugins//- /$'\n'}; do
+        noroot wp plugin install "${plugin}" --path="/srv/www/${domain}/public_html" --activate
+      done
+    fi
 fi
