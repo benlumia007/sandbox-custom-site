9980aa7885d07dae34dac2342b7eb657ee52fad5
Benjamin Lu <benlumia007@users.noreply.github.com>
2019-06-21 10:17:41 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,13 +9,11 @@ title=`get_config_value 'title' "${domain}.test"`
 if [[ "${type}" != "none" ]]; then
     # This should create the basic .conf file for a specific site when it is doing a provision.
     if [[ ! -f /etc/nginx/conf.d/${domain}.conf ]]; then
-      echo "copying nginx.conf to /etc/etc/nginx/conf.d/${domain}.conf"
       cp "/srv/config/nginx/nginx.conf" "/etc/nginx/conf.d/${domain}.conf"
       sed -i -e "s/{{DOMAIN}}/${domain}/g" "/etc/nginx/conf.d/${domain}.conf"
     fi
 
     if [[ ! -d ${vm_dir}/public_html ]]; then
-        echo "Creating ${vm_dir}/public_html"
         mkdir -p ${vm_dir}/public_html
     fi
 
